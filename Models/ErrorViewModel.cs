e2d3deccce5cc0a3e4ea94c0908e0f0964d6508e
Faraz Siddiqui <faraz841@gmail.com>
2021-02-12 16:50:59 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebCheckApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
